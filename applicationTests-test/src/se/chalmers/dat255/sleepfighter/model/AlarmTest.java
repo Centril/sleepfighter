a5eeff03d8c94d28be58723df02de7520facf602
Eric Arnebäck <erkastina@hotmail.com>
2013-09-16 17:12:30 +0200

    Added test project.

---
+++ applicationTests-test/src/se/chalmers/dat255/sleepfighter/model/AlarmTest.java
@@ -0,0 +1,14 @@
+package se.chalmers.dat255.sleepfighter.model;
+
+import junit.framework.TestCase;
+
+public class AlarmTest extends TestCase {
+
+
+	public void test() {
+		Alarm alarm;
+		
+		assertEquals(1, 1);
+	}
+	
+}
