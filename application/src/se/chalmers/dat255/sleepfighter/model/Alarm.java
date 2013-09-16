12493dd09c0451b47fb68a855e4cf7f8f4103fc5
Johan Hasselqvist <u.j.hasselqvist@gmail.com>
2013-09-16 16:56:25 +0200

    Filling the Alarm class with basic methods

--- application/src/se/chalmers/dat255/sleepfighter/model/Alarm.java
+++ application/src/se/chalmers/dat255/sleepfighter/model/Alarm.java
@@ -1,5 +1,37 @@
 package se.chalmers.dat255.sleepfighter.model;
 
 public class Alarm {
+	
+	private boolean isActivated;
+	private int hour;
+	private int minute;
 
+	public Alarm(int hour, int minute) {
+		this.hour = hour;
+		this.minute = minute;
+	}
+	
+	public void setTime(int hour, int minute) {
+		if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+			throw new IllegalArgumentException();
+		}
+		this.hour = hour;
+		this.minute = minute;
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+	
+	public void setActivated(boolean isActivated) {
+		this.isActivated = isActivated;
+	}
+	
+	public boolean isActivated() {
+		return this.isActivated;
+	}
 }
