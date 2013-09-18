d4971fb7b2996441ce15156cb7555e2dca576d76
Eric Arnebäck <erkastina@hotmail.com>
2013-09-18 20:14:01 +0200

    Tested constructor and setTime() of class Alarm.

--- applicationTests-test/src/se/chalmers/dat255/sleepfighter/model/AlarmTest.java
+++ applicationTests-test/src/se/chalmers/dat255/sleepfighter/model/AlarmTest.java
@@ -1,14 +1,41 @@
 package se.chalmers.dat255.sleepfighter.model;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class AlarmTest extends TestCase {
 
 
-	public void test() {
-		Alarm alarm;
+	public void testConstructor() {
+		Alarm alarm = new Alarm(4, 3);
 		
-		assertEquals(1, 1);
+		assertEquals(4, alarm.getHour());
+		assertEquals(3, alarm.getMinute());
 	}
 	
+	public void testSetTime() {
+		Alarm alarm = new Alarm(4, 3);
+		alarm.setTime(3, 4);
+		
+		assertEquals(3, alarm.getHour());
+		assertEquals(4, alarm.getMinute());
+	}
+	
+	public void testSetTimeExceptions() {
+		Alarm alarm = new Alarm(4, 3);
+		
+		// invalid hour
+		try {
+			alarm.setTime(-1, 4);
+			Assert.fail("Should have thrown IllegalArgumentException");
+		} catch(IllegalArgumentException e) {
+			// success
+		}
+		try {
+			alarm.setTime(24, 4);
+			Assert.fail("Should have thrown IllegalArgumentException");
+		} catch(IllegalArgumentException e) {
+			// success
+		}
+	}
 }
