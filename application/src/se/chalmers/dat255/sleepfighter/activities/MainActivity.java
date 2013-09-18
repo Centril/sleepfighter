cb0821e7e6ff99e804c3824e3a2f6d90722a3db6
Niklas Helmertz <niklas.helmertz@gmail.com>
2013-09-18 16:04:58 +0200

    Fill ListView with example entries

--- application/src/se/chalmers/dat255/sleepfighter/activities/MainActivity.java
+++ application/src/se/chalmers/dat255/sleepfighter/activities/MainActivity.java
@@ -1,9 +1,14 @@
 package se.chalmers.dat255.sleepfighter.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import se.chalmers.dat255.sleepfighter.R;
+import se.chalmers.dat255.sleepfighter.model.Alarm;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ListView listView = (ListView) findViewById(R.id.listview);
+
+		// Hard code in some sample alarms
+		// TODO fetch from where actual alarms will be stored
+		List<Alarm> alarms = new ArrayList<Alarm>();
+		alarms.add(new Alarm(8, 30));
+		alarms.add(new Alarm(7, 0));
+		
+		AlarmAdapter alarmAdapter = new AlarmAdapter(this, alarms);
+		listView.setAdapter(alarmAdapter);
 	}
 
 	@Override
