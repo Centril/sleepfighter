ce257c45ad97b05160904b1a077d1ffaf1d7f8a3
Mazdak Farrokhzad <twingoow@gmail.com>
2013-09-19 03:32:41 +0200

    Implemented DateTextUtils, made GUI changes to show "ETA".

--- application/src/se/chalmers/dat255/sleepfighter/activities/MainActivity.java
+++ application/src/se/chalmers/dat255/sleepfighter/activities/MainActivity.java
@@ -1,15 +1,21 @@
 package se.chalmers.dat255.sleepfighter.activities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import se.chalmers.dat255.sleepfighter.R;
 import se.chalmers.dat255.sleepfighter.model.Alarm;
 import se.chalmers.dat255.sleepfighter.model.AlarmsManager;
+import se.chalmers.dat255.sleepfighter.model.AlarmsManager.EarliestInfo;
+import se.chalmers.dat255.sleepfighter.utils.DateTextUtils;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -18,19 +24,30 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		ListView listView = (ListView) findViewById(R.id.listview);
+		Resources res = getResources();
 
 		// Hard code in some sample alarms
 		// TODO fetch from where actual alarms will be stored
+		Calendar now = new GregorianCalendar();
 		List<Alarm> alarms = new ArrayList<Alarm>();
 		alarms.add(new Alarm(8, 30));
 		alarms.add(new Alarm(7, 0));
+		alarms.add(new Alarm(3, 0));
+		alarms.add(new Alarm(13, 37));
 
 		AlarmsManager manager = new AlarmsManager();
 		manager.set( alarms );
 
+		ListView listView = (ListView) findViewById(R.id.mainAlarmsList);
+
+
 		AlarmAdapter alarmAdapter = new AlarmAdapter(this, alarms);
 		listView.setAdapter(alarmAdapter);
+
+		// Set earliest time text.
+		EarliestInfo earliestInfo = manager.getEarliestInfo( now );
+		TextView earliestTimeText = (TextView) findViewById( R.id.earliestTimeText );
+		earliestTimeText.setText( DateTextUtils.getEarliestText( res, now, earliestInfo ) );
 	}
 
 	@Override
---
+++ application/src/se/chalmers/dat255/sleepfighter/utils/DateTextUtils.java
@@ -0,0 +1,74 @@
+package se.chalmers.dat255.sleepfighter.utils;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.List;
+
+import se.chalmers.dat255.sleepfighter.R;
+import se.chalmers.dat255.sleepfighter.model.AlarmsManager.EarliestInfo;
+import android.content.res.Resources;
+import android.util.Log;
+
+/**
+ * String/text specific date utility methods.
+ *
+ * @author Centril<twingoow@gmail.com> / Mazdak Farrokhzad.
+ * @version 1.0
+ * @since Sep 19, 2013
+ */
+public class DateTextUtils {
+	/**
+	 * Construction forbidden.
+	 */
+	private DateTextUtils() {
+	}
+
+	/**
+	 * Builds and returns earliest text when given a resources bundle.
+	 *
+	 * @param res Android resources.
+	 * @return the built earliest text string.
+	 */
+	public static final String getEarliestText( Resources res, Calendar now, EarliestInfo earliestInfo ) {
+		String earliestText;
+
+		// Fetch formats.
+		String[] formats = res.getStringArray( R.array.earliest_time_formats );
+
+		// Not real? = we don't have any alarms active.
+		if ( earliestInfo.isReal() ) {
+			// Compute diff.
+			Calendar diff = earliestInfo.getDiff( now );
+			int[] diffVal = { diff.get( Calendar.DAY_OF_MONTH ), diff.get( Calendar.HOUR_OF_DAY ), diff.get( Calendar.MINUTE ) };
+
+			// What fields are set?
+			BitSet setFields = new BitSet(3);
+			setFields.set( 0, diffVal[0] != 0 );
+			setFields.set( 1, diffVal[1] != 0 );
+			setFields.set( 2, diffVal[2] != 0 );
+			int cardinality = setFields.cardinality();
+
+			earliestText = formats[cardinality + 1];
+
+			if ( cardinality > 0 ) {
+				// We need some pre-formatting done, compile arguments.
+				String[] partFormats = res.getStringArray( R.array.earliest_time_formats_parts );
+				List<String> args = new ArrayList<String>(3);
+
+				for (int i = setFields.nextSetBit(0); i >= 0; i = setFields.nextSetBit(i + 1) ) {
+					args.add( partFormats[i] );
+				}
+
+				// Finally format everything.
+				earliestText = String.format( earliestText, args.toArray() );
+				Log.d( "formatting", earliestText );
+				earliestText = String.format( earliestText, diffVal[0], diffVal[1], diffVal[2] );
+			}
+		} else {
+			earliestText = formats[0];
+		}
+
+		return earliestText;
+	}
+}
