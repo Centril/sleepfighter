d7881c48d4a0cfda3e4b15083dcf9dcfffb8b972
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-09-23 19:01:54 +0200

    Adding the timepicker to the alarm settings

---
+++ application/src/se/chalmers/dat255/sleepfighter/TimepickerPreference.java
@@ -0,0 +1,54 @@
+package se.chalmers.dat255.sleepfighter;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TimePicker;
+
+public class TimepickerPreference extends DialogPreference {
+	
+	private int hour;
+	private int minute;
+	private TimePicker tp;
+	
+	public TimepickerPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		setPositiveButtonText("Okay");
+		setNegativeButtonText("Cancel");
+	}
+	
+	@Override
+	protected View onCreateDialogView() {
+		tp = new TimePicker(getContext());
+		tp.setIs24HourView(true);
+		return(tp);
+	}
+
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		super.onDialogClosed(positiveResult);
+		
+		if (positiveResult) {
+			hour = tp.getCurrentHour();
+			minute = tp.getCurrentMinute();
+		}
+		
+		String time = (hour < 10 ? "0" : "") + hour + ":"
+				+ (minute < 10 ? "0" : "") + minute;
+		
+		
+		if (callChangeListener(time)) {
+            persistString(time);
+        }
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+	
+	public int getMinute() {
+		return minute;
+	}
+}
--- application/src/se/chalmers/dat255/sleepfighter/activities/AlarmSettingsActivity.java
+++ application/src/se/chalmers/dat255/sleepfighter/activities/AlarmSettingsActivity.java
@@ -1,58 +1,106 @@
 package se.chalmers.dat255.sleepfighter.activities;
 
-import se.chalmers.dat255.sleepfighter.R;
-import se.chalmers.dat255.sleepfighter.R.layout;
-import se.chalmers.dat255.sleepfighter.R.menu;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
 
-public class AlarmSettingsActivity extends Activity {
+import java.util.List;
+
+import se.chalmers.dat255.sleepfighter.R;
+import se.chalmers.dat255.sleepfighter.R.string;
+import se.chalmers.dat255.sleepfighter.R.xml;
+import se.chalmers.dat255.sleepfighter.TimepickerPreference;
+
+public class AlarmSettingsActivity extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_alarm_settings);
-		// Show the Up button in the action bar.
-		setupActionBar();
-	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.alarm_settings, menu);
-		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		setupSimplePreferencesScreen();
+	}
+
+	private void setupSimplePreferencesScreen() {
+		addPreferencesFromResource(R.xml.pref_alarm_general);
+
+		bindPreferenceSummaryToValue(findPreference("pref_alarm_time"));
+		bindPreferenceSummaryToValue(findPreference("pref_alarm_name"));
+	}
+
+	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+			String stringValue = value.toString();
+
+			if (preference instanceof TimepickerPreference) {
+				TimepickerPreference tpPref = (TimepickerPreference) preference;
+				
+				int hour = tpPref.getHour();
+				int minute = tpPref.getMinute();
+				
+				// TODO: Assign the actual alarm the values
+				
+				preference.setSummary(stringValue);
+			}
+			else {
+				preference.setSummary(stringValue);
+			}
+			return true;
+		}
+	};
+
+	private static void bindPreferenceSummaryToValue(Preference preference) {
+		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+		// TODO: Check if !persistent it works
+		preference.setPersistent(false);			
+		
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(
+				preference,
+				PreferenceManager.getDefaultSharedPreferences(
+						preference.getContext()).getString(preference.getKey(),
+						""));
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	public static class GeneralPreferenceFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.pref_alarm_general);
+
+			bindPreferenceSummaryToValue(findPreference("pref_alarm_time"));
+			bindPreferenceSummaryToValue(findPreference("pref_alarm_name"));
+		}
+	}
 }
