e6545dc8ff3adb31ad0b3e5149f438490fd89393
metalaszlo <metaomega@gmail.com>
2013-09-23 16:43:02 +0200

    Created model, activity and layout file for challenge.

---
+++ application/src/se/chalmers/dat255/sleepfighter/activities/AbstractChallengeActivity.java
@@ -0,0 +1,14 @@
+package se.chalmers.dat255.sleepfighter.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public abstract class AbstractChallengeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+}
