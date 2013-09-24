4be5e0386cfb0db8c641689a5e48848f079e21ef
Danny Lam <dannylam@live.se>
2013-09-24 14:18:32 +0200

    Changed name activity_abstract_challenge.xml
    to activity_alarm_challenge.

--- application/src/se/chalmers/dat255/sleepfighter/activities/AbstractChallengeActivity.java
+++ application/src/se/chalmers/dat255/sleepfighter/activities/AbstractChallengeActivity.java
@@ -4,13 +4,20 @@ import android.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * An abstract activity for the challenges
+ * 
+ * @version 1.0
+ * @since Sep 24, 2013
+ */
+
 public abstract class AbstractChallengeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_item);
-		
+
 	}
 
 }
