d05814c0ba13af85b57afe68605ea1e7b411d004
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-09-24 12:15:59 +0200

    AlarmSettingsActivity now modifies the correct Alarm object

--- application/src/se/chalmers/dat255/sleepfighter/activities/AlarmSettingsActivity.java
+++ application/src/se/chalmers/dat255/sleepfighter/activities/AlarmSettingsActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.NavUtils;
 
 import se.chalmers.dat255.sleepfighter.R;
 import se.chalmers.dat255.sleepfighter.TimepickerPreference;
+import se.chalmers.dat255.sleepfighter.debug.Debug;
 import se.chalmers.dat255.sleepfighter.model.Alarm;
 import se.chalmers.dat255.sleepfighter.model.AlarmList;
 import se.chalmers.dat255.sleepfighter.SFApplication;
@@ -26,11 +27,15 @@ public class AlarmSettingsActivity extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 		
 		// TODO: Check if id is valid
-		//int id = savedInstanceState.getInt("id");
 		AlarmList manager = ((SFApplication) getApplication()).alarms();
+		int id = this.getIntent().getExtras().getInt("id");
 		
-		// TODO: Fix something to fetch an alarm by its unique id
-		//alarm = manager.getFromId(id);
+		// TODO: Fix something to fetch an alarm by its unique id, then remove the following for loop
+		for (int i = 0; i < manager.size(); i++) {
+			if (manager.get(i).getId() == id) {
+				alarm = manager.get(i);
+			}
+		}
 	}
 
 	@Override
@@ -69,15 +74,15 @@ public class AlarmSettingsActivity extends PreferenceActivity {
 				int hour = tpPref.getHour();
 				int minute = tpPref.getMinute();
 				
-				// TODO: Implement when you can fetch alarm
-				// alarm.setTime(hour, minute);
+				// TODO: Check for null?
+				alarm.setTime(hour, minute);
 				
 				preference.setSummary(stringValue);
 			}
 			else {
 				if (NAME.equals(preference.getKey())) {
-					// TODO: Implement when you can fetch alarm
-					// alarm.setName(stringValue);
+					// TODO: Check for null?
+					alarm.setName(stringValue);
 				}
 				preference.setSummary(stringValue);
 			}
@@ -90,6 +95,13 @@ public class AlarmSettingsActivity extends PreferenceActivity {
 
 		// TODO: Check if !persistent it works
 		preference.setPersistent(false);
+		
+		if (NAME.equals(preference.getKey())) {
+			preference.setSummary(alarm.getName());
+		}
+		else if (TIME.equals(preference.getKey())) {
+			preference.setSummary(alarm.getTimeString());
+		}
 		
 		sBindPreferenceSummaryToValueListener.onPreferenceChange(
 				preference,
