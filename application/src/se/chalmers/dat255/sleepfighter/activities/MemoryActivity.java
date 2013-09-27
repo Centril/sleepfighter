72d22fededb139aa5d539f06fd3b7d3e6a1a36a3
Eric Arnebäck <erkastina@hotmail.com>
2013-09-27 11:57:31 +0200

    Figured out how to layout the memory cards.

--- application/src/se/chalmers/dat255/sleepfighter/activities/MemoryActivity.java
+++ application/src/se/chalmers/dat255/sleepfighter/activities/MemoryActivity.java
@@ -5,6 +5,11 @@ import se.chalmers.dat255.sleepfighter.challenges.MemoryChallenge;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.view.View;;
 
 public class MemoryActivity extends Activity {
 
@@ -14,6 +19,21 @@ public class MemoryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_alarm_challenge_memory);
+	
+		GridView gridview = (GridView) findViewById(R.id.memory_gridview);
+		
+		gridview.setAdapter(new MemoryAdapter(this));
+
+		    gridview.setOnItemClickListener(new OnItemClickListener() {
+		        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		            Toast.makeText(MemoryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+		        }
+		    });
+
+		
+		
+		
+		
 		mc = new MemoryChallenge();
 	}
 
