e7471f475729cae2a9a07e8c4ea296b23e40f06a
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-09-27 16:42:41 +0200

    Emptying MultiSelectListPreference

--- application/src/se/chalmers/dat255/sleepfighter/preference/MultiSelectListPreference.java
+++ application/src/se/chalmers/dat255/sleepfighter/preference/MultiSelectListPreference.java
@@ -1,17 +1,9 @@
 package se.chalmers.dat255.sleepfighter.preference;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-
-import se.chalmers.dat255.sleepfighter.utils.debug.Debug;
-
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnMultiChoiceClickListener;
-import android.content.res.TypedArray;
 import android.preference.ListPreference;
 import android.util.AttributeSet;
 
@@ -22,14 +14,11 @@ import android.util.AttributeSet;
  */
 public class MultiSelectListPreference extends ListPreference {
 
-    private static String separator;
-    private static final String DEFAULT_SEPARATOR = "\u0001\u0007\u001D\u0007\u0001";
     private  boolean[] entryChecked;
 
     public MultiSelectListPreference(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         entryChecked = new boolean[getEntries().length];
-        separator = DEFAULT_SEPARATOR;
     }
 
     public MultiSelectListPreference(Context context) {
@@ -56,94 +45,13 @@ public class MultiSelectListPreference extends ListPreference {
         builder.setMultiChoiceItems(entries, entryChecked, listener);
     }
 
-    public static CharSequence[] unpack(CharSequence val) {
-        if (val == null || "".equals(val)) {
-            return new CharSequence[0];
-        } else {
-            return ((String) val).split(separator);
-        }
-    }
-    
-    public static String pack( List<CharSequence> values ) {
-    	return  join(values, separator);
-    }
-    
-    /**
-     * Gets the entries values that are selected
-     * 
-     * @return the selected entries values
-     */
-    public CharSequence[] getCheckedValues() {
-        return unpack(getValue());
-    }
-
     @Override
     protected void onDialogClosed(boolean positiveResult) {
-        List<CharSequence> values = new ArrayList<CharSequence>();
-
-        CharSequence[] entryValues = getEntryValues();
-        if (positiveResult && entryValues != null) {
-            for (int i = 0; i < entryValues.length; i++) {
-                if (entryChecked[i] == true) {
-                    String val = (String) entryValues[i];
-                    values.add(val);
-                }
-            }
-
-            String value = pack(values);
-
-            if (callChangeListener(value)) {
-            	persistString(value);
-            }
-        }
+    	if (positiveResult) {
+    		callChangeListener(entryChecked);
+    	}
     }
 
-    @Override
-    protected Object onGetDefaultValue(TypedArray typedArray, int index) {
-        return typedArray.getTextArray(index);
-    }
-
-    @Override
-    protected void onSetInitialValue(boolean restoreValue,
-            Object rawDefaultValue) {
-        String value = null;
-        CharSequence[] defaultValue;
-        if (rawDefaultValue == null) {
-            defaultValue = new CharSequence[0];
-        } else {
-            defaultValue = (CharSequence[]) rawDefaultValue;
-        }
-        List<CharSequence> joined = Arrays.asList(defaultValue);
-        String joinedDefaultValue = join(joined, separator);
-        if (restoreValue) {
-            value = getPersistedString(joinedDefaultValue);
-        } else {
-            value = joinedDefaultValue;
-        }
-    }
-
-    /**
-     * Joins array of object to single string by separator
-     * 
-     * Credits to kurellajunior on this post
-     * http://snippets.dzone.com/posts/show/91
-     * 
-     * @param iterable
-     *            any kind of iterable ex.: <code>["a", "b", "c"]</code>
-     * @param separator
-     *            separetes entries ex.: <code>","</code>
-     * @return joined string ex.: <code>"a,b,c"</code>
-     */
-    protected static String join(Iterable<?> iterable, String separator) {
-        Iterator<?> oIter;
-        if (iterable == null || (!(oIter = iterable.iterator()).hasNext()))
-            return "";
-        StringBuilder oBuilder = new StringBuilder(String.valueOf(oIter.next()));
-        while (oIter.hasNext())
-            oBuilder.append(separator).append(oIter.next());
-        return oBuilder.toString();
-    }
-    
     public void setEntryChecked(boolean[] entryChecked) {
     	this.entryChecked = entryChecked;
     }
