49c62267b480cc122fa013478fe00900746a573c
Eric Arnebäck <erkastina@hotmail.com>
2013-09-29 17:36:52 +0200

    When selecting two different cards, the user is now given a couple of seconds to view the cards before they are flipped over and hidden again.

--- application/src/se/chalmers/dat255/sleepfighter/challenge/MemoryChallenge.java
+++ application/src/se/chalmers/dat255/sleepfighter/challenge/MemoryChallenge.java
@@ -5,6 +5,7 @@ import se.chalmers.dat255.sleepfighter.activity.ChallengeActivity;
 import se.chalmers.dat255.sleepfighter.adapter.MemoryAdapter;
 import se.chalmers.dat255.sleepfighter.model.Memory;
 import se.chalmers.dat255.sleepfighter.utils.debug.Debug;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
@@ -25,8 +26,17 @@ public class MemoryChallenge implements Challenge, OnItemClickListener {
 	private final static int COLS = 2;
 
 	private final static int ROWS = 3;
+	
 	private int remainingPairs;
 
+	private Handler handler = new Handler();
+	
+	// true when you are waiting for the cards the user has selected to flip over and become hidden again
+	// when the user has selected two cards that are different, the user is given
+	// some seconds to view the cards. 
+	private boolean waitingForCardsToFlipOver = false;
+	
+	
 	private void fadeOutRemove(View v) {
 		// fade out and remove
         v.startAnimation(AnimationUtils.loadAnimation(act, android.R.anim.fade_out));
@@ -37,22 +47,29 @@ public class MemoryChallenge implements Challenge, OnItemClickListener {
 	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
        // Toast.makeText(act, "" + position, Toast.LENGTH_SHORT).show();
       
-        MemoryCardView card = (MemoryCardView)v;
-        
+		
+		if(waitingForCardsToFlipOver) {
+			// we are still waiting, so you can't pick any cards now.
+			return;
+        }
+		
+        final MemoryCardView card = (MemoryCardView)v;
+
         if(flippedCard == card) {
         	// you can't flip a card, and then flip over the same card again.
         	// you must pick two separate cards. 
         	return;
         }
-        
-        
-        
+       
         card.flip();
         
         if(flippedCard != null ) {
        
         	if(mem.getCard(card.getPosition())  == mem.getCard(flippedCard.getPosition())) {
         		// remove cards
+        		
+        		
+        		
         		fadeOutRemove(card);
         		fadeOutRemove(flippedCard);
         		flippedCard = null;
@@ -63,10 +80,17 @@ public class MemoryChallenge implements Challenge, OnItemClickListener {
         			// TODO: now we should return to the start menu. 
         		}
         	} else {
-        		// TODO: wait some seconds 
-        		flippedCard.flip();
-        		card.flip();
-        		flippedCard = null;
+        	   	waitingForCardsToFlipOver = true;
+        		handler.postDelayed(new Runnable() {
+                	// wait one second before flipping over
+                    public void run() {
+                		flippedCard.flip();
+                		card.flip();
+                		flippedCard = null; 
+                		waitingForCardsToFlipOver = false;
+                    }
+                }, 1000);
+     
         	}
         		 
         } else 
