c009fc991b91e6208f418083f721cee62fe0c3f9
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-09-30 18:08:02 +0200

    Now painting a moving ball

--- application/src/se/chalmers/dat255/sleepfighter/challenge/SnakeChallenge.java
+++ application/src/se/chalmers/dat255/sleepfighter/challenge/SnakeChallenge.java
@@ -1,162 +1,182 @@
 package se.chalmers.dat255.sleepfighter.challenge;
 
-import java.util.Deque;
-import java.util.List;
-import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
-import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
-import android.view.View;
 import se.chalmers.dat255.sleepfighter.activity.ChallengeActivity;
 
-public class SnakeChallenge extends Thread implements Challenge {
+public class SnakeChallenge implements Challenge {
 
-	
-	
-	private long delay = 1000000000L / 25;
-	  private long beforeTime = 0;
-	  private long afterTime = 0;
-	  private long timeDiff = 0;
-	  private long sleepTime;
-	  private long overSleepTime = 0;
-	  private long excess = 0;
-	
-	
-	
-	
-	
-	private boolean playing;
 	private Model model;
-	private SurfaceView view;
-	private Activity activity;
-	
-	private Paint p;
 	
 	@Override
 	public void start(ChallengeActivity activity) {
-		
-		playing = true;
-		this.activity = activity;
-		Display display = activity.getWindowManager().getDefaultDisplay();
-		
-		final int width = display.getWidth();
-		final int height = display.getHeight();
-		
 		model = new Model();
-		
-		p = new Paint();
-		Random r = new Random();
-		p.setColor(new Color().rgb(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-		
-		view = new SurfaceView(activity) implements SurfaceHolder.Callback {
-			
-			@Override
-			public void surfaceCreated (SurfaceHolder holder) {
-		       start();
-		    }
-		};
+		activity.setContentView(new GameView(activity));
 	}
-	
-	@Override
-	public void run() {
-		while (playing) {
-			Canvas c = null;
-		     try {
-		           //lock canvas so nothing else can use it
-		           c = view.getHolder().lockCanvas(null);
-		           synchronized (view.getHolder()) {
-		                //clear the screen with the black painter.
-		        	   c.drawCircle(model.exit.x, model.exit.y, 50, p);
-		                //This is where we draw the game engine.
-		         }
+
+	public class GameView extends SurfaceView implements Callback {
+		// used to keep track of time between updates and amount of time to
+		// sleep for
+		long lastUpdate = 0;
+		long sleepTime = 0;
+
+		// objects which house info about the screen
+		SurfaceHolder surfaceHolder;
+		Context context;
+
+		// our Thread class which houses the game loop
+		private GameThread thread;
+
+		// initialization code
+		void InitView() {
+			// initialize our screen holder
+			SurfaceHolder holder = getHolder();
+			holder.addCallback(this);
+
+			// initialize our Thread class. A call will be made to start it
+			// later
+			thread = new GameThread(holder, context, new Handler());
+			setFocusable(true);
 		}
-		     finally {
-	         // do this in a finally so that if an exception is thrown
-	         // during the above, we don't leave the Surface in an
-	         // inconsistent state
-	         if (c != null) {
-	             view.getHolder().unlockCanvasAndPost(c);
-	         }
-		     }
-	}}
-	
-	private class Position {
-		private int x;
-		private int y;
-		
-		public Position(int x, int y) {
-			this.x = x;
-			this.y = y;
+
+		// class constructors
+		public GameView(Context context) {
+			super(context);
+			this.context = context;
+			InitView();
+		}
+
+		@Override
+		public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2,
+				int arg3) {
+		}
+
+		@Override
+		public void surfaceDestroyed(SurfaceHolder arg0) {
+			boolean retry = true;
+			// code to end gameloop
+			thread.setRunning(false);
+			while (retry) {
+				try {
+					// code to kill Thread
+					thread.join();
+					retry = false;
+				} catch (InterruptedException e) {
+				}
+			}
+
+		}
+
+		@Override
+		public void surfaceCreated(SurfaceHolder arg0) {
+			if (thread.isRunning()) {
+				// When game is opened again in the Android OS
+				thread = new GameThread(getHolder(), context, new Handler());
+				thread.start();
+			} else {
+				// creating the game Thread for the first time
+				thread.start();
+			}
+		}
+	}
+
+	private class GameThread extends Thread {
+		private SurfaceHolder mSurfaceHolder;
+		private Paint mLinePaint;
+		private Paint blackPaint;
+
+		// for consistent rendering
+		private long sleepTime;
+		// amount of time to sleep for (in milliseconds)
+		private long delay = 70;
+
+		private boolean isRunning = true;
+
+		public GameThread(SurfaceHolder surfaceHolder, Context context,
+				Handler handler) {
+
+			// data about the screen
+			mSurfaceHolder = surfaceHolder;
+
+			// standard game painter. Used to draw on the canvas
+			mLinePaint = new Paint();
+			mLinePaint.setARGB(255, 0, 255, 0);
+			// black painter below to clear the screen before the game is
+			// rendered
+			blackPaint = new Paint();
+			blackPaint.setARGB(255, 0, 0, 0);
+			// mLinePaint.setAntiAlias(true);
+
+		}
+
+		public void setRunning(boolean isRunning) {
+			this.isRunning = isRunning;
 		}
 		
-		public void setX(int x) {
-			this.x = x;
+		public boolean isRunning() {
+			return isRunning;
 		}
 		
-		public int getX() {
-			return x;
-		}
-		
-		public void setY(int y) {
-			this.y = y;
-		}
-		
-		public int getY() {
-			return y;
+		@Override
+		public void run() {
+			while (isRunning) {
+				long beforeTime = System.nanoTime();
+				model.update();
+				Canvas c = null;
+				try {
+					c = mSurfaceHolder.lockCanvas(null);
+					synchronized (mSurfaceHolder) {
+						c.drawRect(0, 0, c.getWidth(), c.getHeight(),
+								blackPaint);
+						model.draw(c);
+					}
+				} finally {
+					if (c != null) {
+						mSurfaceHolder.unlockCanvasAndPost(c);
+					}
+				}
+
+				this.sleepTime = delay - ((System.nanoTime() - beforeTime) / 1000000L);
+
+				try {
+					if (sleepTime > 0) {
+						this.sleep(sleepTime);
+					}
+				} catch (InterruptedException ex) {
+					Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
 		}
 	}
 	
 	private class Model {
+		private int x;
+		private int y;
 		
-		private final int START_LENGTH = 5;
-		
-		private Deque<Position> snakePos;
-		private List<Position> emptyPos;
-		private List<Position> obstaclePos;
-		private Position exit;
+		private Paint snakePaint = new Paint();
 		
 		public Model() {
-			exit = new Position(3, 3);
+			x = 20;
+			y = 200;
+			
+			snakePaint.setColor(Color.rgb(255, 255, 0));
+		}
+		
+		public void draw(Canvas c) {
+			c.drawCircle(x, y, 50, snakePaint);
 		}
 		
 		public void update() {
-			exit.setX(exit.getX() + 1);
-		}	
-	}
-	
-	private class DerpView extends SurfaceView implements Callback {
-
-		public DerpView(Context context) {
-			super(context);
-			// TODO Auto-generated constructor stub
+			x += 20;
 		}
-
-		@Override
-		public void surfaceChanged(SurfaceHolder holder, int format, int width,
-				int height) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void surfaceCreated(SurfaceHolder holder) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void surfaceDestroyed(SurfaceHolder holder) {
-			// TODO Auto-generated method stub
-			
-		}
-		
 	}
 }
