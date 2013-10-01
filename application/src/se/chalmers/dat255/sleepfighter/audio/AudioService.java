3866d1c80686b2cd376942759e93a71e06e7bda5
Niklas Helmertz <niklas.helmertz@gmail.com>
2013-10-01 15:26:37 +0200

    Updated AudioService

--- application/src/se/chalmers/dat255/sleepfighter/audio/AudioService.java
+++ application/src/se/chalmers/dat255/sleepfighter/audio/AudioService.java
@@ -2,20 +2,23 @@ package se.chalmers.dat255.sleepfighter.audio;
 
 import java.io.IOException;
 
-import se.chalmers.dat255.sleepfighter.utils.debug.Debug;
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Service for handling audio playback using a MediaPlayer.
  */
 public class AudioService extends Service implements OnPreparedListener,
 		OnErrorListener {
+
+	private static final String TAG = "AudioService";
 
 	public static final String ACTION_PLAY = "se.chalmers.dat255.sleepfighter.audio.AudioService.PLAY";
 	public static final String ACTION_STOP = "se.chalmers.dat255.sleepfighter.audio.AudioService.STOP";
@@ -35,6 +38,8 @@ public class AudioService extends Service implements OnPreparedListener,
 		super.onCreate();
 
 		this.player = new MediaPlayer();
+		this.player.setAudioStreamType(AudioManager.STREAM_ALARM);
+		this.player.setLooping(true);
 		this.state = State.Stopped;
 		this.player.setOnPreparedListener(this);
 		this.player.setOnErrorListener(this);
@@ -43,13 +48,18 @@ public class AudioService extends Service implements OnPreparedListener,
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		String action = intent.getAction();
+		Log.d(TAG, "start action " + action);
+
 		if (ACTION_PLAY.equals(action)) {
-			// TODO null checks etc
-			String uriString = intent.getStringExtra(BUNDLE_URI);
-			Uri uri = Uri.parse(uriString);
+			Object o = intent.getParcelableExtra(BUNDLE_URI);
+			if(!(o instanceof Uri)) {
+				throw new IllegalArgumentException("No Uri bundled with PLAY action");
+			}
+			Uri uri = (Uri) o;
 			play(uri);
 		} else if (ACTION_STOP.equals(action)) {
-			stop();
+			stopPlayback();
+			stopSelf();
 		}
 		return START_NOT_STICKY;
 	}
@@ -61,45 +71,65 @@ public class AudioService extends Service implements OnPreparedListener,
 	 *            the uri
 	 */
 	private void play(Uri uri) {
-		// TODO handle exception differently
+		// Stop if previously playing
+		if (state != State.Stopped) {
+			stopPlayback();
+		}
+
 		try {
 			player.setDataSource(this, uri);
 			player.prepareAsync();
+			state = State.Preparing;
 		} catch (IllegalArgumentException e) {
-			Debug.e(e);
+			handleException(e);
 		} catch (SecurityException e) {
-			Debug.e(e);
+			handleException(e);
 		} catch (IllegalStateException e) {
-			Debug.e(e);
+			handleException(e);
 		} catch (IOException e) {
-			Debug.e(e);
+			handleException(e);
 		}
 	}
 
 	@Override
 	public void onPrepared(MediaPlayer mp) {
 		if (state == State.Preparing) {
-			state = State.Playing;
-			mp.setLooping(true);
 			mp.start();
+			state = State.Playing;
 		}
 	}
 
 	/**
-	 * Stop any ongoing audio playback.
+	 * Stops any ongoing audio playback.
 	 */
-	private void stop() {
+	private void stopPlayback() {
 		if (state == State.Playing) {
 			player.stop();
 		}
 		player.reset();
 		state = State.Stopped;
-		stopSelf();
+	}
+
+	@Override
+	public void onDestroy() {
+		stopPlayback();
+		super.onDestroy();
+	}
+
+	/**
+	 * Handle an exception by stopping and logging.
+	 * 
+	 * @param e the exception
+	 */
+	private void handleException(Exception e) {
+		stopPlayback();
+		Log.e(TAG, "AudioService exception", e);
 	}
 
 	@Override
 	public boolean onError(MediaPlayer mp, int what, int extra) {
-		stop();
+		Log.e(TAG, "MediaPlayer error = (" + what + ", " + extra + ")");
+		stopPlayback();
 		return true;
 	}
 
