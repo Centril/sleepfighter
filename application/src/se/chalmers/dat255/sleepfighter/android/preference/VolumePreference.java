f8f72a81d3ce848d500d38d54a1053c9b616d738
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-10-04 15:07:57 +0200

    Adding license

--- application/src/se/chalmers/dat255/sleepfighter/android/preference/VolumePreference.java
+++ application/src/se/chalmers/dat255/sleepfighter/android/preference/VolumePreference.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2013 See AUTHORS file.
+ * 
+ * This file is part of SleepFighter.
+ * 
+ * SleepFighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SleepFighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SleepFighter. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package se.chalmers.dat255.sleepfighter.android.preference;
 
 import android.content.Context;
@@ -6,8 +25,15 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.SeekBar;
 
+/**
+ * A preference designed to change volume for a specific alarm
+ * 
+ * @author Hassel
+ *
+ */
 public class VolumePreference extends DialogPreference {
 
+	// maximum number of points the slider can stick to
 	private final int maxNbr = 7;
 	
 	SeekBar slider;
