1ed49063e2c0470e935d73e69dbcac94639d6fd4
Eric Arnebäck <erkastina@hotmail.com>
2013-10-05 22:21:47 +0200

    Removed SquareLayout. To replace it, there is a new xml attribute "scale" for RectangleLayout.

--- application/src/se/chalmers/dat255/sleepfighter/android/component/layout/RectangleLayout.java
+++ application/src/se/chalmers/dat255/sleepfighter/android/component/layout/RectangleLayout.java
@@ -1,6 +1,8 @@
 package se.chalmers.dat255.sleepfighter.android.component.layout;
 
+import se.chalmers.dat255.sleepfighter.R;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
@@ -11,14 +13,35 @@ public class RectangleLayout extends LinearLayout {
 	public RectangleLayout( Context context ) {
 		super( context );
 	}
+	
+	private double mScale;
+
 
 	public RectangleLayout( Context context, AttributeSet attrs ) {
 		super( context, attrs );
+
+
+		// We have added a custom xml attribute "scale" to this class. This code
+		// reads that attribute.
+		TypedArray a = context.obtainStyledAttributes(attrs,R.styleable.RectangleLayout);
+		final int N = a.getIndexCount();
+		for (int i = 0; i < N; ++i)
+		{
+			int attr = a.getIndex(i);
+			switch (attr)
+			{
+			case R.styleable.RectangleLayout_scale:
+				this.mScale = Double.parseDouble(a.getString(attr));
+				break;
+
+			}
+		}
+		a.recycle();
 	}
 
 	@Override
 	protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
-		double mScale = 0.5; // because 4/2 = 2
+//		double mScale = 0.5; // because 4/2 = 2
 
 		int width = MeasureSpec.getSize( widthMeasureSpec );
 		int height = MeasureSpec.getSize( heightMeasureSpec );
