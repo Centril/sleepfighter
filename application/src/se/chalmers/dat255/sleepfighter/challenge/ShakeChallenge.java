13fd73002541d57552e4d42c713fd09b9d4098be
Niklas Helmertz <niklas.helmertz@gmail.com>
2013-10-05 15:57:28 +0200

    Added licence and Javadoc

--- application/src/se/chalmers/dat255/sleepfighter/challenge/ShakeChallenge.java
+++ application/src/se/chalmers/dat255/sleepfighter/challenge/ShakeChallenge.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2013 See AUTHORS file.
+ * 
+ * This file is part of SleepFighter.
+ * 
+ * SleepFighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SleepFighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SleepFighter. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package se.chalmers.dat255.sleepfighter.challenge;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
@@ -13,6 +31,9 @@ import android.widget.TextView;
 import se.chalmers.dat255.sleepfighter.R;
 import se.chalmers.dat255.sleepfighter.activity.ChallengeActivity;
 
+/**
+ * A challenge where the user have to shake the device to complete it.
+ */
 public class ShakeChallenge implements Challenge {
 
 	private static final float GOAL = 5000;
