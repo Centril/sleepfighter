53543874eb0cb86096c77e2fb8ed22672444084d
Niklas Helmertz <niklas.helmertz@gmail.com>
2013-10-09 15:18:53 +0200

    Added "play playlist" action in service
    Maybe not the best solution to handle playlist logic in the service, but
    difficult elsewhere because of the need of a \"on complete\" callback,
    which is easily accessed in the Service.

--- application/src/se/chalmers/dat255/sleepfighter/audio/AudioService.java
+++ application/src/se/chalmers/dat255/sleepfighter/audio/AudioService.java
@@ -18,12 +18,11 @@
  ******************************************************************************/
 package se.chalmers.dat255.sleepfighter.audio;
 
-import java.io.IOException;
-
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
@@ -35,56 +34,91 @@ import android.util.Log;
  * Service for handling audio playback using a MediaPlayer.
  */
 public class AudioService extends Service implements OnPreparedListener,
-		OnErrorListener {
+		OnErrorListener, OnCompletionListener {
 
 	private static final String TAG = "AudioService";
 
 	/**
-	 * Action for starting playback of local media.
-	 * <p>A {@link Uri}, to identify the audio track in the Android MediaStore,
-	 * must be bundled with the Intent, using the name defined by
-	 * {@link #BUNDLE_URI}. In addition to that must the initial volume, a float
-	 * (0-1), also be bundled with the intent, using the name defined by
-	 * {@link #BUNDLE_FLOAT_VOLUME}.</p>
+	 * Action for starting playback of a single audio track.
+	 * <p>Required extras:</p>
+	 * <ul>
+	 * <li>A {@link Uri}, using name defined by {@link #BUNDLE_URI} - the Uri
+	 * for the track to be played</li>
+	 * <li>A float (0-1), using name defined by {@link #BUNDLE_FLOAT_VOLUME} -
+	 * the initial volume</li>
+	 * </ul>
 	 */
 	public static final String ACTION_PLAY = "se.chalmers.dat255.sleepfighter.audio.AudioService.PLAY";
-	
+
 	/**
 	 * Action for stopping any playback.
 	 */
 	public static final String ACTION_STOP = "se.chalmers.dat255.sleepfighter.audio.AudioService.STOP";
 
 	/**
-	 * Action for modifying the volume of playing audio.
-	 * <p>The volume, a float (0-1), to use for playback must be bundled with the
-	 * Intent, using the name defined by {@link #BUNDLE_FLOAT_VOLUME}.</p>
+	 * Action for modifying the volume of what's currently playing. 
+	 * <p>Required extras:</p>
+	 * <ul>
+	 * <li>A float, using name defined by {@link #BUNDLE_FLOAT_VOLUME} - the
+	 * volume (0-1) for the playing audio</li>
+	 * </ul>
 	 */
 	public static final String ACTION_VOLUME = "se.chalmers.dat255.sleepfighter.audio.AudioService.VOLUME";
 
+	/**
+	 * Action for starting playback of a playlist.
+	 * <p>Required extras:</p>
+	 * <ul>
+	 * <li>A {@code String[]}, using name defined by {@link #BUNDLE_PLAYLIST} -
+	 * paths to the tracks to be played</li>
+	 * <li>A float (0-1), using name defined by {@link #BUNDLE_FLOAT_VOLUME} -
+	 * the initial volume</li>
+	 * </ul>
+	 */
+	public static final String ACTION_PLAY_PLAYLIST = "se.chalmers.dat255.sleepfighter.audio.AudioService.PLAY_PLAYLIST";
+
 	public static final String BUNDLE_URI = "audio_uri";
 	public static final String BUNDLE_FLOAT_VOLUME = "audio_volume";
+	public static final String BUNDLE_PLAYLIST = "audio_playlist";
 
-	public enum State {
+	/**
+	 * The states the MediaPlayer can be in.
+	 */
+	private enum State {
 		PREPARING,
 		PLAYING,
 		STOPPED
 	}
 
+	/**
+	 * The available sources this can play from.
+	 */
+	private enum SourceType {
+		SINGLE,
+		PLAYLIST
+	}
+
 	private MediaPlayer player;
 	private State state;
+	private SourceType sourceType;
+
+	// Variables only relevant when source is playlist
+	// Might be better if handled elsewhere
+	private String[] tracks;
+	private int track;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		this.player = new MediaPlayer();
 		this.player.setAudioStreamType(AudioManager.STREAM_ALARM);
-		this.player.setLooping(true);
 		this.player.setOnPreparedListener(this);
 		this.player.setOnErrorListener(this);
+		this.player.setOnCompletionListener(this);
 
 		// Makes MediaPlayer hold a wake lock while playing
 		this.player.setWakeMode(this, PowerManager.PARTIAL_WAKE_LOCK);
-		
+
 		this.state = State.STOPPED;
 	}
 
@@ -100,17 +134,64 @@ public class AudioService extends Service implements OnPreparedListener,
 			handleStopAction();
 		} else if (ACTION_VOLUME.equals(action)) {
 			handleVolumeAction(intent);
+		} else if (ACTION_PLAY_PLAYLIST.equals(action)) {
+			handleVolumeAction(intent);
+			handlePlaylistPlay(intent);
 		}
 		return START_NOT_STICKY;
 	}
 
 	private void handlePlayAction(Intent intent) {
 		Object o = intent.getParcelableExtra(BUNDLE_URI);
-		if(!(o instanceof Uri)) {
-			throw new IllegalArgumentException("No Uri bundled with PLAY action");
+		if (!(o instanceof Uri)) {
+			throw new IllegalArgumentException(
+					"No Uri bundled with PLAY action");
 		}
 		Uri uri = (Uri) o;
-		play(uri);
+
+		// Stop if previously playing
+		if (this.state != State.STOPPED) {
+			stopPlayback();
+		}
+
+		setSourceType(SourceType.SINGLE);
+
+		// Loop if only one track
+		this.player.setLooping(true);
+		try {
+			this.player.setDataSource(this, uri);
+		} catch (Exception e) {
+			handleException(e);
+			return;
+		}
+		prepareAndPlay();
+	}
+
+	private void handlePlaylistPlay(Intent intent) {
+		Object o = intent.getStringArrayExtra(BUNDLE_PLAYLIST);
+		if (!(o instanceof String[])) {
+			throw new IllegalArgumentException(
+					"No String[] bundled with PLAY_PLAYLIST action");
+		}
+		String[] tracks = (String[]) o;
+		if (tracks.length == 0) {
+			throw new IllegalArgumentException(
+					"Empty String[] bundled with PLAY_PLAYLIST action");
+		}
+
+		// Stop if previously playing
+		if (this.state != State.STOPPED) {
+			stopPlayback();
+		}
+
+		setSourceType(SourceType.PLAYLIST);
+		this.tracks = tracks;
+
+		// Don't let player loop, handle ourselves in onCompletion
+		this.player.setLooping(false);
+
+		setPlaylistDataSource();
+		prepareAndPlay();
 	}
 
 	private void handleStopAction() {
@@ -123,46 +204,62 @@ public class AudioService extends Service implements OnPreparedListener,
 		setVolume(volume);
 	}
 
-	private void setVolume(float volume) {
-		if (volume > 1 || volume < 0) {
-			throw new IllegalArgumentException("No valid volume bundled");
-		}
-
-		this.player.setVolume(volume, volume);
-	}
-
 	/**
-	 * Starts playback of audio referenced by the given uri.
-	 * 
-	 * @param uri
-	 *            the uri
+	 * Prepares playback of the data source set on MediaPlayer asynchronously
+	 * and starts playback when prepared.
 	 */
-	private void play(Uri uri) {
-		// Stop if previously playing
-		if (state != State.STOPPED) {
-			stopPlayback();
-		}
-
+	private void prepareAndPlay() {
 		try {
-			player.setDataSource(this, uri);
-			player.prepareAsync();
-			state = State.PREPARING;
-		} catch (IllegalArgumentException e) {
-			handleException(e);
-		} catch (SecurityException e) {
-			handleException(e);
-		} catch (IllegalStateException e) {
-			handleException(e);
-		} catch (IOException e) {
+			this.player.prepareAsync();
+			this.state = State.PREPARING;
+		} catch (Exception e) {
 			handleException(e);
 		}
 	}
 
 	@Override
 	public void onPrepared(MediaPlayer mp) {
-		if (state == State.PREPARING) {
+		// Don't start if state no longer is preparing
+		if (this.state == State.PREPARING) {
 			mp.start();
-			state = State.PLAYING;
+			this.state = State.PLAYING;
+		}
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		this.state = State.STOPPED;
+		mp.reset();
+		if (this.sourceType == SourceType.PLAYLIST) {
+			incTrack();
+			prepareAndPlay();
+		} else {
+			throw new RuntimeException("Non playlist complete");
+		}
+	}
+
+	/**
+	 * Increases the playlist track.
+	 */
+	private void incTrack() {
+		if (this.sourceType != SourceType.PLAYLIST) {
+			throw new RuntimeException(
+					"Non playlist source type can not use incTrack");
+		}
+		// Increase track by one and wrap around to first if at last
+		this.track = (this.track + 1) % this.tracks.length;
+		setPlaylistDataSource();
+	}
+
+	/**
+	 * Sets the data source of the player to the current track in the playlist,
+	 * defined by {@link #track}.
+	 */
+	private void setPlaylistDataSource() {
+		try {
+			this.player.setDataSource(this.tracks[this.track]);
+		} catch (Exception e) {
+			handleException(e);
 		}
 	}
 
@@ -170,34 +267,67 @@ public class AudioService extends Service implements OnPreparedListener,
 	 * Stops any ongoing audio playback.
 	 */
 	private void stopPlayback() {
-		if (state == State.PLAYING) {
-			player.stop();
+		if (this.state == State.PLAYING) {
+			this.player.stop();
 		}
-		player.reset();
-		state = State.STOPPED;
+		this.player.reset();
+
+		if (this.sourceType == SourceType.PLAYLIST) {
+			// Reset playlist-only variables
+			this.tracks = null;
+			this.track = 0;
+		}
+
+		this.state = State.STOPPED;
+
+		setSourceType(null);
+	}
+
+	/**
+	 * Sets the source type for the player.
+	 * 
+	 * @param sourceType
+	 *            the source type
+	 */
+	private void setSourceType(SourceType sourceType) {
+		this.sourceType = sourceType;
+	}
+
+	/**
+	 * Sets the volume for the audio played through the MediaPlayer.
+	 * 
+	 * @param volume
+	 *            the volume as a float 0-1
+	 */
+	private void setVolume(float volume) {
+		if (volume > 1 || volume < 0) {
+			throw new IllegalArgumentException("No valid volume bundled");
+		}
+		this.player.setVolume(volume, volume);
 	}
 
 	@Override
 	public void onDestroy() {
 		stopPlayback();
-		player.release();
+		this.player.release();
 		super.onDestroy();
 	}
 
 	/**
 	 * Handle an exception by stopping and logging.
 	 * 
-	 * @param e the exception
+	 * @param exception
+	 *            the exception
 	 */
-	private void handleException(Exception e) {
+	private void handleException(Exception exception) {
 		stopPlayback();
-		Log.e(TAG, "AudioService exception", e);
+		Log.e(TAG, "AudioService exception", exception);
 	}
 
 	@Override
 	public boolean onError(MediaPlayer mp, int what, int extra) {
-		Log.e(TAG, "MediaPlayer error = (" + what + ", " + extra + ")");
 		stopPlayback();
+		Log.e(TAG, "MediaPlayer error = (" + what + ", " + extra + ")");
 		return true;
 	}
 
