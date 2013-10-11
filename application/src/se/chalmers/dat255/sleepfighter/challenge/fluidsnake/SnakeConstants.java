34889b1bea3fe1c219dadddc8864bfc3cfecb441
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-10-11 12:50:36 +0200

    Moving constants to its own class

---
+++ application/src/se/chalmers/dat255/sleepfighter/challenge/fluidsnake/SnakeConstants.java
@@ -0,0 +1,38 @@
+package se.chalmers.dat255.sleepfighter.challenge.fluidsnake;
+
+public class SnakeConstants {
+
+	// How fast the game should try to update and render
+	public static final int TARGET_FPS = 60;
+	
+	// Size of the model board
+	public static final int BOARD_WIDTH = 200;
+	public static final int BOARD_HEIGHT = 400;
+	
+	// Multiplier for how fast the game will run in the model
+	public static final float SPEED_MULT = 2;
+	
+	// Number if circles the snake will be made up by
+	public static final int SEGMENTS = 6;
+	// radius of the snake segments
+	
+	public static final int SNAKE_WIDTH = 15;
+	
+	// Starting point for the snake
+	public static final int START_X = BOARD_WIDTH/2;
+	public static final int START_Y = 300;
+	
+	// How wide the obstacles (walls) will be
+	public static final int OBSTACLE_WIDTH = 15;
+	// How far into the obstacle the snake can go before dying
+	public static final int OBSTACLE_MARGIN = 8;
+	
+	// Number of fruits to pick before exit is revealed
+	public static final int FRUITS = 3;
+	
+	// Radius of the sphere fruits
+	public static final int FRUIT_WIDTH = 10;
+	
+	// Don't create an instance of this class
+	private SnakeConstants() { }
+}
