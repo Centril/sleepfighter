fe6f6b1fba95cd3d740e6cef7e15c91c99f3e5e1
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2013-10-11 12:53:22 +0200

    Moving the FPS to the constants class as well

--- application/src/se/chalmers/dat255/sleepfighter/challenge/fluidsnake/FluidSnakeChallenge.java
+++ application/src/se/chalmers/dat255/sleepfighter/challenge/fluidsnake/FluidSnakeChallenge.java
@@ -35,100 +35,109 @@ import android.widget.Toast;
 /**
  * A fluid snake challenge.
  * 
- * Touch on the screen to choose the direction to where the snake is moving.
- * Eat all red fruits to open an exit which you will need to pass through in order to win.
- * If you hit a wall or one of the snake segments you will lose and the game will restart.
+ * Touch on the screen to choose the direction to where the snake is moving. Eat
+ * all red fruits to open an exit which you will need to pass through in order
+ * to win. If you hit a wall or one of the snake segments you will lose and the
+ * game will restart.
  * 
  * @author Hassel
- *
+ * 
  */
 public class FluidSnakeChallenge implements Challenge, OnTouchListener {
 	/**
 	 * PrototypeDefinition for FluidSnakeChallenge.
-	 *
+	 * 
 	 * @version 1.0
 	 * @since Oct 5, 2013
 	 */
-	public static class PrototypeDefinition extends ChallengePrototypeDefinition {{
-		setType( ChallengeType.FLUID_SNAKE );
-	}}
+	public static class PrototypeDefinition extends
+			ChallengePrototypeDefinition {
+		{
+			setType(ChallengeType.FLUID_SNAKE);
+		}
+	}
 
 	private Model model;
 	private Thread thread;
 	private GameView view;
 	private ChallengeActivity activity;
-	
+
 	// where the user touched on the screen
 	private float touchX;
 	private float touchY;
-	
+
 	// true if we should call the updateDirection method
 	private boolean updateDir;
-	
-	public final int targetFPS = 60;
-	
+
 	@Override
 	public void start(ChallengeActivity activity, ChallengeResolvedParams params) {
 		this.activity = activity;
 
 		activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		
+
 		activity.setTitle(R.string.fluidSnakeTitle);
-		
+
 		model = new Model();
 		view = new GameView(activity, model);
-		
+
 		view.setOnTouchListener(this);
 		activity.setContentView(view);
-		
+
 		thread = new Thread() {
 			@Override
 			public void run() {
 				long lastTime = 0;
-				
+
 				// run as long as you havent won
 				while (!model.won()) {
-					
+
 					// if the player lost, restart the game
 					if (model.lost()) {
 						restart();
 					}
-					
-					// calculate delta in order to make the game run smoothly even if
-					// the target FPS isn't reached (or if it is actually higher than supposed)
+
+					// calculate delta in order to make the game run smoothly
+					// even if
+					// the target FPS isn't reached (or if it is actually higher
+					// than supposed)
 					long now = System.currentTimeMillis();
-					float delta = (lastTime > 0 ? now - lastTime : 1000/targetFPS)/(1000f/targetFPS);
+					float delta = (lastTime > 0 ? now - lastTime
+							: 1000 / SnakeConstants.TARGET_FPS)
+							/ (1000f / SnakeConstants.TARGET_FPS);
 					lastTime = now;
-					
-					
+
 					// update the direction of the snake only when needed
 					if (updateDir) {
-						model.updateDirection(touchX/view.getWidth(), touchY/view.getHeight());
+						model.updateDirection(touchX / view.getWidth(), touchY
+								/ view.getHeight());
 						updateDir = false;
 					}
-					
-					// update the snake model, with the provided delta (actually a multiplier and not really a delta)
+
+					// update the snake model, with the provided delta (actually
+					// a multiplier and not really a delta)
 					model.update(delta);
-					
+
 					// render the view
 					view.render();
-					
+
 					try {
-						Thread.sleep(1000/targetFPS);
+						Thread.sleep(1000 / SnakeConstants.TARGET_FPS);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
-				
-				// if we got out of the loop it means the player won, complete the challenge
+
+				// if we got out of the loop it means the player won, complete
+				// the challenge
 				exitChallenge();
 			}
 		};
 		thread.start();
 	}
-	
+
 	private void exitChallenge() {
-		// Run on UI thread since things can get messy if trying to execute them on other threads
+		// Run on UI thread since things can get messy if trying to execute them
+		// on other threads
 		activity.runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
@@ -136,7 +145,7 @@ public class FluidSnakeChallenge implements Challenge, OnTouchListener {
 			}
 		});
 	}
-	
+
 	/**
 	 * Provides the view with a new model
 	 */
@@ -144,22 +153,24 @@ public class FluidSnakeChallenge implements Challenge, OnTouchListener {
 		model = new Model();
 		view.setModel(model);
 	}
-	
+
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		touchX = event.getX();
 		touchY = event.getY();
-		
+
 		// update the direction in the next game loop
 		updateDir = true;
-		
+
 		return true;
 	}
 
 	@Override
-	public void start( ChallengeActivity activity, ChallengeResolvedParams params, Bundle state ) {
-		Toast.makeText( activity, "TODO: IMPLEMENT START FROM SAVED STATE", Toast.LENGTH_LONG ).show();
-		this.start( activity, params );
+	public void start(ChallengeActivity activity,
+			ChallengeResolvedParams params, Bundle state) {
+		Toast.makeText(activity, "TODO: IMPLEMENT START FROM SAVED STATE",
+				Toast.LENGTH_LONG).show();
+		this.start(activity, params);
 	}
 
 	@Override
