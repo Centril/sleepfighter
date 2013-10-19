2b9c63c04c59fb3b99452eeb0dda985c31d42229
Eric Arnebäck <erkastina@hotmail.com>
2013-10-19 09:36:01 +0200

    Refactored createRandomMatrix to MatrixUtil.

--- application/src/se/chalmers/dat255/sleepfighter/utils/math/MatrixUtil.java
+++ application/src/se/chalmers/dat255/sleepfighter/utils/math/MatrixUtil.java
@@ -1,6 +1,11 @@
 package se.chalmers.dat255.sleepfighter.utils.math;
 
+import java.util.Random;
+
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 public class MatrixUtil {
 	
@@ -26,5 +31,28 @@ public class MatrixUtil {
 		double i = data[2][2];
 		
 		return a*e*i+b*f*g+c*d*h-c*e*g-b*d*i-a*f*h;
+	}
+	
+	// create a random quadratic matrix, with random integers. MIN_INT and MAX_INT is the range of these random integers. 
+	public static RealMatrix createRandomMatrix(final Random rng, final int size, final int MIN_INT, final int MAX_INT) {
+
+		double[][] matrixData = new double[size][size];
+		
+		for(int i = 0; i < size; ++i) {
+			for(int j = 0; j < size; ++j) {
+				matrixData[i][j] =   RandomMath.nextRandomRanged(rng, MIN_INT, MAX_INT);
+			}
+		}
+		
+		return MatrixUtils.createRealMatrix(matrixData);
+	}
+	
+	// Create a random 3d vector
+	public static RealVector createRandomVector(final Random rng, final int size, final int MIN_INT, final int MAX_INT) {
+		return new ArrayRealVector(new double[]{
+				RandomMath.nextRandomRanged(rng, MIN_INT, MAX_INT),
+				RandomMath.nextRandomRanged(rng, MIN_INT, MAX_INT),
+				RandomMath.nextRandomRanged(rng, MIN_INT, MAX_INT)
+		});
 	}
 }
