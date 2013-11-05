4a0eb50b65d17da147f41772ea1cb723a2cfb207
Centril <twingoow@gmail.com>
2013-11-05 02:34:20 +0100

    Made ReflectionUtil.

---
+++ application/src/se/toxbee/sleepfighter/utils/reflect/ReflectionUtil.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2013 See AUTHORS file.
+ * 
+ * This file is part of SleepFighter.
+ * 
+ * SleepFighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SleepFighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SleepFighter. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package se.toxbee.sleepfighter.utils.reflect;
+
+import java.lang.reflect.Array;
+
+/**
+ * ReflectionUtil provides very basic reflection utilities.
+ *
+ * @author Centril<twingoow@gmail.com> / Mazdak Farrokhzad.
+ * @version 1.0
+ * @since Nov 5, 2013
+ */
+public class ReflectionUtil {
+	/**
+	 * Creates an array of type T of size.
+	 *
+	 * @param clazz the Class object of type T.
+	 * @param size the size of array.
+	 * @return the array.
+	 */
+	@SuppressWarnings( "unchecked" )
+	public static <T> T[] makeArray( Class<T> clazz, int size ) {
+		return (T[]) Array.newInstance( clazz, size );
+	}
+
+	/**
+	 * Returns the Class object for an element of a generic array.
+	 *
+	 * @param arr the array.
+	 * @return the Class object.
+	 */
+	@SuppressWarnings( "unchecked" )
+	public static <T> Class<T> arrayClass( T[] arr ) {
+		return (Class<T>) arr.getClass().getComponentType();
+	}
+}
