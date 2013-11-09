96f75774ff7df887358fcf53332bdb677f82fe75
Centril <twingoow@gmail.com>
2013-11-09 06:03:34 +0100

    Added logic for parsing minesweeper config from string + level logic.

--- application/src/se/toxbee/sleepfighter/challenge/minesweeper/MinesweeperConfig.java
+++ application/src/se/toxbee/sleepfighter/challenge/minesweeper/MinesweeperConfig.java
@@ -18,8 +18,13 @@
  ******************************************************************************/
 package se.toxbee.sleepfighter.challenge.minesweeper;
 
+import java.util.Iterator;
+
+import com.google.common.base.Preconditions;
+
 import se.toxbee.sleepfighter.utils.geom.Dimension;
 import se.toxbee.sleepfighter.utils.geom.FinalPosition;
+import se.toxbee.sleepfighter.utils.string.StringUtils;
 
 /**
  * MinesweeperConfig models a configuration of how to make a minesweeper board.
@@ -29,12 +34,97 @@ import se.toxbee.sleepfighter.utils.geom.FinalPosition;
  * @since Nov 5, 2013
  */
 public final class MinesweeperConfig {
-	public static final MinesweeperConfig BEGINNER		= new MinesweeperConfig( 8, 8, 10 );
-	public static final MinesweeperConfig INTERMEDIATE	= new MinesweeperConfig( 16, 16, 40 );
-	public static final MinesweeperConfig EXPERT		= new MinesweeperConfig( 30, 16, 99 );
+	/**
+	 * Level defines standard levels based on Microsoft's Minesweeper.
+	 *
+	 * @author Centril<twingoow@gmail.com> / Mazdak Farrokhzad.
+	 * @version 1.0
+	 * @since Nov 9, 2013
+	 */
+	public enum Level {
+		BEGINNER( 8, 8, 10 ),
+		INTERMEDIATE( 16, 16, 40 ),
+		EXPERT( 30, 16, 99 );
 
-	private final Dimension dim;
-	private final int mineCount;
+		Level( int w, int h, int mines ) {
+			this.cfg = new MinesweeperConfig( w, h, mines );
+		}
+
+		/**
+		 * Returns the config for the level.
+		 *
+		 * @return the config.
+		 */
+		public MinesweeperConfig getConfig() {
+			return this.cfg;
+		}
+
+		/**
+		 * Returns a level from an integer.<br/>
+		 * If no level corresponds to the argument,<br/>
+		 * {@link #BEGINNER} is returned.
+		 *
+		 * @param i the level corresponding to {@link #ordinal()}.
+		 * @return the corresponding level.
+		 */
+		public static Level fromInt( int i ) {
+			i = Math.abs( i );
+
+			Level[] levels = values();
+			return	i < levels.length
+				?	levels[i]
+				:	BEGINNER; // Houston, we've a problem - there's no such level, resort to lowest (beginner).
+		}
+
+		private MinesweeperConfig cfg;
+	}
+
+	/**
+	 * Creates a config from a string in the format "{w},{h},{m}"<br/>
+	 * where {w} = width, {h} = height, {m} = mines in board.<br/>
+	 * All values are expected to be integral.<br/>
+	 * The format is forgiving: any whitespace in between values are omitted.
+	 *
+	 * @param str the string containing arguments.
+	 * @return the created config.
+	 */
+	public static MinesweeperConfig fromString( String str ) {
+		if ( str == null || str.isEmpty() ) {
+			throw new IllegalArgumentException( "String was empty/null, can't make config" );
+		}
+
+		/*
+		 * We make the following assumptions:
+		 * 1) The user intends positive values, abs(x) them.
+		 * 3) [w] = [1
+		 * 2) Anything else = fail.
+		 *
+		 * In other words: we're a bit forgiving.
+		 */
+
+		// First pass, split.
+		Iterator<String> it = StringUtils.COMMA_SPLITTER.split( str ).iterator();
+
+		if ( it.hasNext() ) {
+			int w = iterNextInt( it );
+	
+			if ( it.hasNext() ) {
+				int h = iterNextInt( it );
+
+				if ( it.hasNext() ) {
+					int m = iterNextInt( it );
+
+					return new MinesweeperConfig( w, h, m );
+				}
+			}
+		}
+
+		throw new IllegalArgumentException( "Params string is invalid! Given: " + str );
+	}
+
+	private static int iterNextInt( Iterator<String> it ) {
+		return Math.abs( Integer.parseInt( it.next() ) );
+	}
 
 	/**
 	 * Constructs a MinesweeperConfig with (width, height) and mineCount.
@@ -54,6 +144,16 @@ public final class MinesweeperConfig {
 	 * @param mineCount the mine count of boards made with this config.
 	 */
 	public MinesweeperConfig( Dimension dim, int mineCount ) {
+		if ( dim.n() != 2 ) {
+			throw new IllegalArgumentException( "2D dim required, given:" + dim.n() );
+		} else if ( Preconditions.checkNotNull( dim ).lowest() >= 3 ) {
+			throw new IllegalArgumentException( "min(dim) >= 3 required." );
+		} else if ( mineCount < 1 ) {
+			throw new IllegalArgumentException( "mineCount >= 1 required, given: " + mineCount );
+		} else if ( dim.cross() - mineCount < (3 * 4 - 1) ) {
+			throw new IllegalArgumentException( "There must be space for atleast: " + (3 * 4 - 1) + " free cells." );
+		}
+
 		this.dim = dim;
 		this.mineCount = mineCount;
 	}
@@ -75,4 +175,7 @@ public final class MinesweeperConfig {
 	public int mineCount() {
 		return this.mineCount;
 	}
+
+	private final Dimension dim;
+	private final int mineCount;
 }
