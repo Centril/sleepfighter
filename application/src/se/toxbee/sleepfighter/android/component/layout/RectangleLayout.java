44f850a7cf2b6aa5374c3df160d7a2e1bfa2fd12
Centril <twingoow@gmail.com>
2013-11-11 19:08:35 +0100

    Added constructor to RectangleLayout.

--- application/src/se/toxbee/sleepfighter/android/component/layout/RectangleLayout.java
+++ application/src/se/toxbee/sleepfighter/android/component/layout/RectangleLayout.java
@@ -10,27 +10,29 @@ import android.widget.LinearLayout;
  * Used for laying out a rectangular layout.
  */
 public class RectangleLayout extends LinearLayout {
-	public RectangleLayout( Context context ) {
-		super( context );
-	}
-	
 	private double mScale;
 
+	public RectangleLayout( Context context ) {
+		this( context, 1.0 );
+	}
+
+	public RectangleLayout( Context context, double scale ) {
+		super( context );
+		this.mScale = scale;
+	}
 
 	public RectangleLayout( Context context, AttributeSet attrs ) {
 		super( context, attrs );
 
-
 		// We have added a custom xml attribute "scale" to this class. This code
 		// reads that attribute.
-		TypedArray a = context.obtainStyledAttributes(attrs,R.styleable.RectangleLayout);
+		TypedArray a = context.obtainStyledAttributes( attrs, R.styleable.RectangleLayout );
 		final int N = a.getIndexCount();
-		for (int i = 0; i < N; ++i)
-		{
-			int attr = a.getIndex(i);
-			
-			if(attr == R.styleable.RectangleLayout_scale) {
-				this.mScale = Double.parseDouble(a.getString(attr));
+		for ( int i = 0; i < N; ++i ) {
+			int attr = a.getIndex( i );
+
+			if ( attr == R.styleable.RectangleLayout_scale ) {
+				this.mScale = Double.parseDouble( a.getString( attr ) );
 			}
 		}
 		a.recycle();
