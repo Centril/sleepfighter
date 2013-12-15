0506bec0a434bd6981ff372f42a5b4c0dbae099a
Centril <twingoow@gmail.com>
2013-12-15 02:01:11 +0100

    Moved android.settings -> android.preference.

--- application/src/se/toxbee/sleepfighter/android/preference/SharedPreferenceManager.java
+++ application/src/se/toxbee/sleepfighter/android/preference/SharedPreferenceManager.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with SleepFighter. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package se.toxbee.sleepfighter.android.settings;
+package se.toxbee.sleepfighter.android.preference;
 
 import java.io.Serializable;
 import java.util.Map;
